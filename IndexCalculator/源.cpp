4d2ace4c75e64baa62daff4996481324250b92e7
hyskylord <26114709+hyskylord@users.noreply.github.com>
2020-03-18 12:55:26 -0400

    debug options

--- IndexCalculator/源.cpp
+++ IndexCalculator/源.cpp
@@ -8,7 +8,7 @@ int main()
 	cout << "please input index:" << endl;
 	cin	>> i  ;
 	cout << "I=" << i << endl;
-	for (j = 1; j < a; j++)  //到达上界后退出
+	for (j = i * i + 2 * i + 3; j < a; j++)  //到达上界后退出
 	{ 
 		result = true;
 		for (k = 1; k <= j; k++)  //对第一个数的分母枚举
@@ -41,8 +41,22 @@ int main()
 								break;
 							}
 						}
+						//debug
+//						for (m = i * i + 2 * i + 3; m <= i * i + 2 * i + 3; m++)
+//						{
+//							if ((b[2] < c[2] && ((m * i + 1) * b[0]) / c[0] + ((m * i + 1) * b[1]) / c[1] + ((m * i + 1) * b[2]) / c[2] <= 2 * m * i) || (b[2] == c[2] && ((m * i + 1) * b[0]) / c[0] + ((m * i + 1) * b[1]) / c[1] + m * i <= 2 * m * i))
+//							{
+//								result = true;
+//								break;
+//							}
+//						}
+						//debug
 						if (result == false)  //不存在m 回到第一个循环处
 						{
+							cout << "j=" << j << endl;
+							cout << "b1=" << b[0] << "/" << c[0] << endl;
+							cout << "b2=" << b[1] << "/" << c[1] << endl;
+							cout << "b3=" << b[2] << "/" << c[2] << endl;       //debug
 							break;
 						}
 					}
